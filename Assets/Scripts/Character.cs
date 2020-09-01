d76df93add7c3f7358c35fac65bd27aa0c9537f4
genie <60090391+blingblin-g@users.noreply.github.com>
2020-09-01 23:13:28 +0900

    init commit
    character class
    game manager
    Scenes

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private string nickName = null;
+    private int level = 0;
+    private int amour = 0;
+    private int hp = 100;
+    private int darkSoul = 0;
+    private int soulBuket = 1;
+    
+    // need skill
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string NickName {
+        get {
+            return this.nickName;
+        }
+        set {
+            this.nickName = value;
+        }
+    }
+
+    public int Level {
+        get {
+            return this.level;
+        }
+        set {
+            this.level = value;
+        }
+    }
+
+    public int Amour {
+        get {
+            return this.amour;
+        }
+
+        set {
+            this.amour = value;
+        }
+    }
+
+    public int Hp {
+        get {
+            return this.hp;
+        }
+        set {
+            this.hp = value;
+        }
+    }
+
+    public int DarkSoul {
+        get {
+            return this.darkSoul;
+        }
+        set {
+            this.darkSoul = value;
+        }
+    }
+
+    public int SoulBuket {
+        get {
+            return this.soulBuket;
+        }
+        set {
+            this.soulBuket = value;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    private int money = 0;
+
+    private void Awake()
+    {
+        if (instance == null) // only one GameManager
+        {
+            instance = this;
+        }
+        else // destroy to preventing from overlapping 
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public int Money
+    {
+        get
+        {
+            return this.money;
+        }
+        set
+        {
+            this.money = value;
+        }
+    }
+}
