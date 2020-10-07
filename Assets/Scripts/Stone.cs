e2c14401323746ad73cfd5c6deea50efe2e63444
genie <60090391+blingblin-g@users.noreply.github.com>
2020-10-07 22:23:28 +0900

    Add respawn stones

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour
 {
+    [SerializeField] private SoulType soulType = SoulType.NONE;
     private Transform characterTransform;
     private float speed = 1f;
     private bool isCollision = false;
+
 
     void Start()
     {
@@ -19,18 +21,22 @@ public class Stone : MonoBehaviour
         if (isCollision)
         {
             transform.Translate((characterTransform.position - transform.position) * Time.deltaTime * speed);
+            if (Vector3.Distance(characterTransform.position, transform.position) < 0.2f)
+            {
+                GameManager.Instance.StoneCounter[soulType] += 1;
+                Destroy(this.gameObject);
+            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Character character = collision.gameObject.GetComponent<Character>();
-        if (character)
+
+        if (character && !isCollision)
         {
             isCollision = true;
             characterTransform = character.transform;
-            Debug.Log("this is Character pos: " + character.transform.position);
         }
     }
-
 }
