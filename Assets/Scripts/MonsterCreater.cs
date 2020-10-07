80225bf37db4ab449c9ee971f1ef1f0ef700f55e
genie <60090391+blingblin-g@users.noreply.github.com>
2020-10-07 23:21:43 +0900

    Add SceneChange

---
+++ Assets/Scripts/MonsterCreater.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCreater : MonoBehaviour
+{
+    [SerializeField] private GameObject[] monsters = null;
+    [SerializeField] private float xMin;
+    [SerializeField] private float xMax;
+    [SerializeField] private float yMin;
+    [SerializeField] private float yMax;
+    float time = 0;
+    float maxTime = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > maxTime)
+        {
+            RandomRespawn();
+            time -= maxTime;
+        }
+    }
+
+    private void RandomRespawn()
+    {
+        int i = StoneFrequency();
+        float x = Random.Range(xMin, xMax);
+        float y = Random.Range(yMin, yMax);
+        Vector3 spawn = new Vector3(x, y, -1);
+        Instantiate(monsters[i], spawn, Quaternion.identity);
+    }
+
+    private int StoneFrequency()
+    {
+        int i = Random.Range(0, 100);
+        if (i < 40)
+            return 0;
+        if (i < 70)
+            return 1;
+        if (i < 90)
+            return 2;
+        else
+            return 3;
+
+    }
+}
