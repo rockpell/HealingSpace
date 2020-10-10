3f9c3268af7d06eda72f8622e6498cb2057c65db
rockpell <aythffk@gmail.com>
2020-10-10 23:21:14 +0900

    Add battle monster

---
+++ Assets/Scripts/BattleMonster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleMonster : MonoBehaviour
+{
+    [SerializeField] private Transform teduri = null;
+    [SerializeField] private Slider monsterHpBar = null;
+    [SerializeField] private Slider characterHpBar = null;
+    [SerializeField] private Button domang = null;
+    [SerializeField] private GameObject item = null; // getting item when take the monster down.
+
+    private float monsterHp = 100;
+    private float characterHp = 100;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
